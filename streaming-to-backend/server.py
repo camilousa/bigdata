1b7f7d19e34c408d22acf909c2b3b412dc8a334c
Camilo Rodriguez <rodriguez.camilo@gmail.com>
2021-05-24 15:17:33 -0500

    simple dashboard interfaces created using chart.js

--- streaming-to-backend/server.py
+++ streaming-to-backend/server.py
@@ -1,13 +1,21 @@
 from flask import Flask
 from flask import request
-app = Flask(__name__)
+from flask_cors import CORS
 
+app = Flask(__name__)
+CORS(app)
+
+data = {}
 @app.route('/', methods=['POST'])
 def receive_data():
     sensor = request.form['sensor']
     count = request.form['count']
     print('Recibido ', sensor, count )
+    data[sensor] = count
     return 'Dato recibido'
 
+@app.route('/dashboard')
+def dashboard():
+    return  data
 
 app.run()
